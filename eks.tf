f01684ced84c13f868adea4a4e8f17b54cb019fe
Amash Ansari <ansari2002ksp@gmail.com>
2024-09-07 18:01:11 +0530

    Created eks.tf

---
+++ eks.tf
@@ -0,0 +1,31 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.eks_name
+  cluster_version = "1.30"
+
+  enable_irsa = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    cluster = "my-eks-cluster"
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t2.micro"]
+    vpc_security_group_ids = [module.vpc.vpc_security_group_ids]
+  }
+
+  eks_managed_node_groups = {
+    node_group = {
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+}
